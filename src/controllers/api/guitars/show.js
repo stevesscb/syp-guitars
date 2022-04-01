00c4ccf1e7cae60bf0129adf22164b04365f106d
stevesscb <stevesscb@gmail.com>
2022-04-01 10:57:16 +0800

    added carousel to home page

--- src/controllers/api/guitars/show.js
+++ src/controllers/api/guitars/show.js
@@ -9,7 +9,8 @@ const controllersApiGuitarShow = async (req, res) => {
     const foundGuitar = await prisma.Guitar.findUnique({
       where: { id: Number(id) },
       include: {
-        user: true
+        user: true,
+        images: true
       }
     })
     return res.status(200).json(foundGuitar)
